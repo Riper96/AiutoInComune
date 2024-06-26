d234609217932c0e85a3518674aaed03cee46e2f
Riper96 <jionson90@live.it>
2024-06-26 17:15:33 +0200

    controllo appli

--- AiutoInComune/src/main/java/com/jaita120/services/ComuneService.java
+++ AiutoInComune/src/main/java/com/jaita120/services/ComuneService.java
@@ -1,5 +1,8 @@
 package com.jaita120.services;
 
+import com.jaita120.entities.Comune;
+
 public interface ComuneService {
 
+	Comune findById(Integer id);
 }
--- AiutoInComune/src/main/java/com/jaita120/services/ComuneServiceImpl.java
+++ AiutoInComune/src/main/java/com/jaita120/services/ComuneServiceImpl.java
@@ -1,8 +1,20 @@
 package com.jaita120.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ComuneServiceImpl {
+import com.jaita120.entities.Comune;
+import com.jaita120.repository.ComuneRepository;
 
+@Service
+public class ComuneServiceImpl implements ComuneService{
+
+	@Autowired
+	private ComuneRepository repoCo;
+
+	@Override
+	public Comune findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
